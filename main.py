91e5448a3bc0ef4bc3ef4c92fe382ed477abb960
Tony Devoto <tonydevoto@gmail.com>
2017-04-19 19:56:31 -0500

    finished user-signup

--- main.py
+++ main.py
@@ -14,83 +14,108 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
 import webapp2
 import cgi
+import re
 
-page_header = """
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Signup</title>
-    <style type="text/css">
-        .error {
-            color: red;
-        }
-    </style>
-</head>
-<body>
-    <h1>
-        <a href="/">Signup</a>
-    </h1>
-"""
-class Index(webapp2.RequestHandler):
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+	return USER_RE.match(username)
 
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+	return PASS_RE.match(password)
 
-    def get(self):
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+def valid_email(email):
+	return EMAIL_RE.match(email)
 
-        user_form = """
-        <form action="/add" method="post">
-            <label>
-                Username
-                <input type="text" name="username"/>
+def forms(user_username, username_error, password_error, verify_error, user_email, email_error):
+	headers = ("<head>"
+		"<title>Sign Up</title>"
+		"<style type='text/css'>"
+			".error {color: red}"
+		"</style>"
+		"</head>"
 
-            </label>
-            <br>
-            <label>
-                Password
-                <input type="text" name="password"/>
-
-            </label>
-            <br>
-            <label>
-                Verify Password
-                <input type="text" name="verify"/>
-
-            </label>
-            <br>
-            <label>
-                Email (optional)
-                <input type="text" name="email"/>
-
-            </label>
-            <br>
-            <input type="submit" value="Submit"/>
-        </form>
-        """
-        error = self.request.get("error")
-        if error:
-            error_esc = cgi.escape(error, quote=True)
-            error_element = "<p class='error'>" + error_esc + "</p>"
-        else:
-            error_element = ""
-
-        # combine all the pieces to build the content of our response
-        content = page_header + user_form + error_element
-        self.response.write(content)
-
-class Sub(webapp2.RequestHandler):
+        "<h2>Signup</h2>")
 
 
-    def post(self):
-        edit_header = "<h3>Welcome</h3>"
-        # look inside the request to figure out what the user typed
-        user = self.request.get("username")
+	tables = ("<table>"
+		"<tr>"
+			"<td>Username</td>"
+			"<td><input type='text' name='username' value='" + user_username + "'></input></td>"
+			"<td class='error'>" + username_error + "</td>"
+		"</tr>"
 
+		"<tr>"
+			"<td>Password</td>"
+			"<td><input type='password' name='password'></input></td>"
+			"<td class='error'>" + password_error + "</td>"
+		"</tr>"
 
-        content = edit_header + user
-        self.response.write(content)
+		"<tr>"
+			"<td>Verify Password</td>"
+			"<td><input type='password' name='password_verify'></input></td>"
+			"<td class='error'>" + verify_error + "</td>"
+		"</tr>"
+
+		"<tr>"
+			"<td>Email (optional)</td>"
+			"<td><input type='text' name='email' value='" + user_email + "'></input></td>"
+			"<td class='error'>" + email_error + "</td>"
+		"</tr>"
+	"</table>")
+
+	submit_button = "<input type='submit'/>"
+
+	form = headers + "<form method='post'>" + tables + submit_button + "</form>"
+
+	return form
+
+class MainHandler(webapp2.RequestHandler):
+	def get(self):
+		content = forms("", "", "", "", "", "")
+		self.response.write(content)
+
+        def post(self):
+		errorcount = 0
+		username = self.request.get("username")
+		password = self.request.get("password")
+		verify = self.request.get("password_verify")
+		email = self.request.get("email")
+		username_error = ""
+		password_error = ""
+		verify_error = ""
+		email_error = ""
+
+		if not username or not valid_username(username):
+			username_error = "Username not valid."
+			errorcount += 1
+		if not password or not valid_password(password):
+			password_error = "That's not a valid password."
+			errorcount += 1
+		if password != verify:
+			verify_error = "The passwords don't match."
+			errorcount += 1
+		if  email != '' and not valid_email(email):
+			email_error = "That's not a valid email address."
+			errorcount += 1
+
+		if errorcount > 0:
+			content = forms(username, username_error, password_error, verify_error, email, email_error)
+			self.response.write(content)
+		else:
+			self.redirect("/welcome?username=" + username)
+
+class Welcome(webapp2.RequestHandler):
+	def get(self):
+		username = self.request.get("username")
+		header = "<h2>Welcome, " + username + "!" + "</h2>"
+		self.response.write(header)
 
 app = webapp2.WSGIApplication([
-    ('/', Index),
-    ('/add', Sub)
+	('/', MainHandler),
+	('/welcome', Welcome)
 ], debug=True)
